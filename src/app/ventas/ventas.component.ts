49fc7dd2beaded9b49aa8578761484916a6cf074
Fernando Rios <luis.fer334.fa@gmail.com>
2021-11-19 21:17:46 -0300

    cambios que checkean el todo list de matias

--- src/app/ventas/ventas.component.ts
+++ src/app/ventas/ventas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { Clientes } from '../model/clientes';
 import { Venta } from '../model/venta';
 
@@ -25,12 +26,15 @@ export class VentasComponent implements OnInit {
   agregarProducto(){
     // push a copy of detalle into detalles
     this.detalles.push(Object.assign({}, this.detalle));
-    this.detalle = {producto:"", cantidad:0, total:0};
     let total = 0;
     this.detalles.forEach(detalle => {
       total = total + detalle.total;
     });
     this.venta.total = total;
+    this.detalle = {producto:"", cantidad:0, total:0};
+
+    const input:any = document.getElementsByName('cantidad');
+    input.item(0).value = 0;
   }
 
   eliminarProducto(producto){
@@ -43,5 +47,25 @@ export class VentasComponent implements OnInit {
     const ventas = JSON.parse(localStorage.getItem('ventas')) || [];
     ventas.push(this.venta);
     localStorage.setItem('ventas', JSON.stringify(ventas));
+    Swal.fire({
+      title: 'Guardado!',
+      text: 'La venta se guardo exitosamente.',
+      icon: 'success',
+      customClass: {
+      confirmButton: 'btn btn-success',
+      },
+      buttonsStyling: false,
+    }).then(() => {
+      this.venta = new Venta();
+      this.venta.fecha = new Date();
+    });
+  }
+
+  cantidadHandler(e){
+    console.log(e.target.value);
+    const product = this.productos.find(producto => producto.nombre === this.detalle.producto);
+    this.detalle.cantidad = e.target.value;
+    this.detalle.total = this.detalle.cantidad * product?.precioVenta;
+
   }
 }
